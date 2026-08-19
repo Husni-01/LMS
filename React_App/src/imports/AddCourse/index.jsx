b6ed8f71e5abd6fe44df38efedef63523d99c967
Husni-01 <husniatham@gmail.com>
2026-08-19 09:37:16 +0530

    Added import for LMS project

---
+++ React_App/src/imports/AddCourse/index.jsx
@@ -0,0 +1,346 @@
+import svgPaths from "./svg-8eife63011";
+import imgImage79 from "./2f98443600853eb28ceee6bce5c996dca9bbb140.png";
+import imgHowToCreateTextToImageGeneratorFullStackSaasAppUsingReactJs1 from "./7c9cc70470b97f61b3a2f32830d72fd6de1c2932.png";
+import { imgImage78 } from "./svg-ljsc1";
+
+function FacebookIcon() {
+  return (
+    <div className="absolute left-[1170px] size-[35px] top-[2638px]" data-name="facebook_icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="35" preserveAspectRatio="none" viewBox="0 0 35 35" width="35">
+        <g id="facebook_icon">
+          <path d={svgPaths.p2f97c280} fill="var(--fill-0, black)" id="Facebook" />
+          <circle cx="17.5" cy="17.5" id="Ellipse 2803" r="17.25" stroke="var(--stroke-0, #5E5E5E)" strokeWidth="0.5" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function TwitterIcon() {
+  return (
+    <div className="absolute left-[1213px] size-[35px] top-[2638px]" data-name="twitter_icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="35" preserveAspectRatio="none" viewBox="0 0 35 35" width="35">
+        <g id="twitter_icon">
+          <path d={svgPaths.p152fc480} fill="var(--fill-0, black)" id="Twitter" />
+          <circle cx="17.5" cy="17.5" id="Ellipse 2804" r="17.25" stroke="var(--stroke-0, #5E5E5E)" strokeWidth="0.5" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function InstagramIcon() {
+  return (
+    <div className="absolute left-[1256px] size-[35px] top-[2638px]" data-name="instagram_icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="35" preserveAspectRatio="none" viewBox="0 0 35 35" width="35">
+        <g id="instagram_icon">
+          <circle cx="17.5" cy="17.5" id="Ellipse 2805" r="17.25" stroke="var(--stroke-0, #5E5E5E)" strokeWidth="0.5" />
+          <path clipRule="evenodd" d={svgPaths.p27001d00} fill="var(--fill-0, #222222)" fillRule="evenodd" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function HomeIcon() {
+  return (
+    <div className="absolute inset-[3.56%_96.01%_95.8%_2.78%]" data-name="home_icon">
+      <div className="absolute inset-[-5.75%]">
+        <svg className="block size-full" fill="none" height="19.3841" preserveAspectRatio="none" viewBox="0 0 19.3841 19.3841" width="19.3841">
+          <g id="home_icon">
+            <path d={svgPaths.p314b4450} id="Vector" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeWidth="2" />
+            <path d={svgPaths.p34a0fb00} id="Vector_2" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeWidth="2" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function AddIcon() {
+  return (
+    <div className="absolute inset-[5.56%_95.96%_93.77%_2.78%]" data-name="add_icon">
+      <div className="absolute inset-[-5.49%]">
+        <svg className="block size-full" fill="none" height="20.2119" preserveAspectRatio="none" viewBox="0 0 20.2119 20.2119" width="20.2119">
+          <g id="add_icon">
+            <path d={svgPaths.p1ca20700} id="Vector" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeWidth="2" />
+            <path d={svgPaths.p293ae500} id="Vector_2" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeWidth="2" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function MaskGroup() {
+  return (
+    <div className="absolute contents left-[1357px] top-[14px]" data-name="Mask group">
+      <div className="absolute h-[47.411px] left-[1335.3px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[21.697px_0px] mask-size-[45px_45px] top-[14px] w-[69.107px]" style={{ maskImage: `url("${imgImage78}")` }} data-name="image 78">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage79} />
+      </div>
+    </div>
+  );
+}
+
+function UserUser() {
+  return (
+    <div className="absolute inset-[0.96%_3.61%_98.33%_95.21%]" data-name="User / User_02">
+      <div className="absolute inset-[-5.23%_-5.88%]">
+        <svg className="block size-full" fill="none" height="21.125" preserveAspectRatio="none" viewBox="0 0 19 21.125" width="19">
+          <g id="User / User_02">
+            <path d={svgPaths.p3730b900} id="user_icon" stroke="var(--stroke-0, #4A4A4A)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function ProfileIcon() {
+  return (
+    <div className="absolute contents left-[1357px] top-[14px]" data-name="profile_icon">
+      <MaskGroup />
+      <div className="absolute left-[1357px] size-[45px] top-[14px]">
+        <div className="absolute inset-[0_-8.89%_-17.78%_-8.89%]">
+          <svg className="block size-full" fill="none" height="53" preserveAspectRatio="none" viewBox="0 0 53 53" width="53">
+            <g filter="url(#filter0_d_1_1027)" id="Ellipse 2806">
+              <circle cx="26.5" cy="22.5" fill="var(--fill-0, #EBF7FF)" r="22.5" />
+            </g>
+            <defs>
+              <filter colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse" height="53" id="filter0_d_1_1027" width="53" x="0" y="0">
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+                <feOffset dy="4" />
+                <feGaussianBlur stdDeviation="2" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+                <feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_1_1027" />
+                <feBlend in="SourceGraphic" in2="effect1_dropShadow_1_1027" mode="normal" result="shape" />
+              </filter>
+            </defs>
+          </svg>
+        </div>
+      </div>
+      <UserUser />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute inset-[0.67%_94.86%_98.07%_2.78%]">
+      <svg className="absolute block inset-0 size-full" fill="none" height="34" preserveAspectRatio="none" viewBox="0 0 34 34" width="34">
+        <g id="Group 4607">
+          <path clipRule="evenodd" d={svgPaths.p4a849a0} fill="var(--fill-0, #0260FF)" fillRule="evenodd" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="absolute contents left-[40px] top-[18px]" data-name="logo">
+      <Group />
+      <div className="-translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Outfit:SemiBold',sans-serif] font-semibold justify-center leading-[0] left-[80px] text-[#0e0e0e] text-[24px] top-[35px] whitespace-nowrap">
+        <p className="leading-[normal]">Edemy</p>
+      </div>
+    </div>
+  );
+}
+
+function FileUploadIcon() {
+  return (
+    <div className="absolute inset-[16.56%_53.26%_83.04%_45.76%]" data-name="file_upload_icon">
+      <div className="absolute inset-[-7.05%_-5.36%]">
+        <svg className="block size-full" fill="none" height="12.1356" preserveAspectRatio="none" viewBox="0 0 15.5 12.1356" width="15.5">
+          <g id="file_upload_icon">
+            <g id="Vector">
+              <path d={svgPaths.p2f554900} fill="var(--fill-0, #0260FF)" />
+              <path d={svgPaths.p2f554900} stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+            </g>
+            <g id="Vector_2">
+              <path d={svgPaths.p2d63e680} fill="var(--fill-0, #0260FF)" />
+              <path d={svgPaths.p1c252400} stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function ProfileUploadIcon() {
+  return (
+    <div className="absolute contents left-[648px] top-[434px]" data-name="profile_upload_icon">
+      <div className="absolute bg-[#0260ff] left-[648px] rounded-[4px] size-[36px] top-[434px]" />
+      <FileUploadIcon />
+    </div>
+  );
+}
+
+function Group1() {
+  return (
+    <div className="absolute inset-[97.74%_89.03%_1%_8.61%]">
+      <svg className="absolute block inset-0 size-full" fill="none" height="34" preserveAspectRatio="none" viewBox="0 0 34 34" width="34">
+        <g id="Group 4607">
+          <path clipRule="evenodd" d={svgPaths.p4a849a0} fill="var(--fill-0, #0260FF)" fillRule="evenodd" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Logo1() {
+  return (
+    <div className="absolute contents left-[124px] top-[2638px]" data-name="logo">
+      <Group1 />
+      <div className="-translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Outfit:SemiBold',sans-serif] font-semibold justify-center leading-[0] left-[164px] text-[24px] text-black top-[2655px] whitespace-nowrap">
+        <p className="leading-[normal]">Edemy</p>
+      </div>
+    </div>
+  );
+}
+
+function PersonTickIcon() {
+  return (
+    <div className="absolute inset-[9.34%_95.95%_89.96%_2.92%]" data-name="person_tick_icon">
+      <div className="absolute inset-[-5.26%_-6.14%]">
+        <svg className="block size-full" fill="none" height="21" preserveAspectRatio="none" viewBox="0 0 18.2857 21" width="18.2857">
+          <g id="person_tick_icon">
+            <path d={svgPaths.pc797b80} id="patients_icon" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Group2() {
+  return (
+    <div className="absolute flex inset-[7.52%_95.97%_91.83%_2.92%] items-center justify-center" style={{ containerType: "size" }}>
+      <div className="-scale-x-100 flex-none h-[100cqh] w-[100cqw]">
+        <div className="relative size-full">
+          <div className="absolute inset-[-5.18%_-5.63%]">
+            <svg className="block size-full" fill="none" height="19.1909" preserveAspectRatio="none" viewBox="0 0 17.7996 19.1909" width="17.7996">
+              <g id="Group 4812">
+                <path d={svgPaths.p2cab3e00} id="Vector" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.8" />
+                <path d={svgPaths.p1d02500} id="Vector_2" stroke="var(--stroke-0, #1C274C)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.8" />
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function AddCourse() {
+  return (
+    <div className="bg-white relative size-full" data-name="Add Course">
+      <div className="absolute bg-white border-[rgba(37,37,37,0.3)] border-r border-solid h-[932px] left-[-3px] top-0 w-[251px]" />
+      <div className="absolute flex h-[74px] items-center justify-center left-[-3px] top-0 w-[1443px]">
+        <div className="-rotate-90 flex-none">
+          <div className="bg-white border-[rgba(37,37,37,0.3)] border-l border-solid h-[1443px] relative w-[74px]" />
+        </div>
+      </div>
+      <div className="-translate-x-1/2 [word-break:break-word] absolute bottom-[1.07%] flex flex-col font-['Outfit:Regular',sans-serif] font-normal justify-center leading-[0] left-[calc(50%-290.5px)] text-[#797484] text-[16px] text-center top-[97.81%] w-[277px]">
+        <p className="leading-[normal]">All right reserved. Copyright @Edemy</p>
+      </div>
+      <div className="absolute flex h-[25px] items-center justify-center left-[266px] top-[2643px] w-0">
+        <div className="flex-none rotate-90">
+          <div className="h-0 relative w-[25px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" height="1" preserveAspectRatio="none" viewBox="0 0 25 1" width="25">
+                <line id="Line 1836" stroke="var(--stroke-0, #9D9D9D)" x2="25" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FacebookIcon />
+      <TwitterIcon />
+      <InstagramIcon />
+      <div className="absolute bg-[#f2f3ff] h-[47px] left-[-3px] top-[135px] w-[251px]" />
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[3.33%_84.58%_95.55%_4.79%] justify-center leading-[0] text-[#252525] text-[16px]">
+        <p className="leading-[normal]">Dashboard</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[5.34%_84.58%_93.55%_4.79%] justify-center leading-[0] text-[#252525] text-[16px]">
+        <p className="leading-[normal]">Add Course</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[7.3%_84.58%_91.59%_4.79%] justify-center leading-[0] text-[#252525] text-[16px]">
+        <p className="leading-[normal]">My Courses</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[9.15%_84.58%_89.74%_4.79%] justify-center leading-[0] text-[#252525] text-[16px]">
+        <p className="leading-[normal]">{`Student Enrolled `}</p>
+      </div>
+      <div className="absolute bg-[#5f6fff] h-[47px] left-[242px] top-[135px] w-[6px]" />
+      <HomeIcon />
+      <AddIcon />
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[0.78%_5.76%_98.11%_87.57%] justify-center leading-[0] text-[#4a4a4a] text-[15px] text-center">
+        <p>
+          <span className="leading-[normal]">{`Hi! `}</span>
+          <span className="[word-break:break-word] font-['Outfit:Regular',sans-serif] font-normal leading-[normal]">Richard</span>
+        </p>
+      </div>
+      <ProfileIcon />
+      <div className="absolute border border-[rgba(37,37,37,0.5)] border-solid h-[40px] left-[283px] rounded-[4px] top-[129px] w-[476px]" />
+      <div className="absolute border border-[rgba(37,37,37,0.5)] border-solid h-[40px] left-[283px] rounded-[4px] top-[218px] w-[476px]" />
+      <div className="absolute border border-[rgba(37,37,37,0.5)] border-solid h-[40px] left-[283px] rounded-[4px] top-[432px] w-[139px]" />
+      <div className="absolute bg-black border border-[rgba(0,0,0,0.2)] border-solid h-[40px] left-[284px] rounded-[4px] top-[518px] w-[91px]" />
+      <div className="absolute border border-[rgba(37,37,37,0.5)] border-solid h-[82px] left-[283px] rounded-[4px] top-[304px] w-[476px]" />
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[3.63%_69.65%_95.48%_19.72%] justify-center leading-[0] text-[16px] text-[rgba(37,37,37,0.7)]">
+        <p className="leading-[normal]">Course Title</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[6.93%_69.65%_92.18%_19.72%] justify-center leading-[0] text-[16px] text-[rgba(37,37,37,0.7)]">
+        <p className="leading-[normal]">Course Headings</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[16.3%_56.32%_82.81%_34.31%] justify-center leading-[0] text-[16px] text-[rgba(37,37,37,0.7)]">
+        <p className="leading-[normal]">Course Thumbnail</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[14.86%_69.65%_84.25%_19.72%] justify-center leading-[0] text-[16px] text-[rgba(37,37,37,0.7)]">
+        <p className="leading-[normal]">Course Price</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[10.11%_69.72%_89%_19.65%] justify-center leading-[0] text-[16px] text-[rgba(37,37,37,0.7)]">
+        <p className="leading-[normal]">Course Description</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[5.08%_68.82%_94.03%_20.56%] justify-center leading-[0] text-[14px] text-[rgba(37,37,37,0.5)]">
+        <p className="leading-[normal]">Type here</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[8.37%_68.82%_90.74%_20.56%] justify-center leading-[0] text-[14px] text-[rgba(37,37,37,0.5)]">
+        <p className="leading-[normal]">Type here</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[16.3%_72.57%_82.81%_20.56%] justify-center leading-[0] text-[14px] text-[rgba(37,37,37,0.5)]">
+        <p className="leading-[normal]">0</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[19.49%_75.9%_79.62%_21.67%] justify-center leading-[0] text-[16px] text-white">
+        <p className="leading-[normal]">ADD</p>
+      </div>
+      <div className="[word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal inset-[11.56%_68.82%_87.55%_20.56%] justify-center leading-[0] text-[14px] text-[rgba(37,37,37,0.5)]">
+        <p className="leading-[normal]">Type here</p>
+      </div>
+      <div className="absolute h-[98px] left-[1208px] top-[79px] w-[194px]" data-name="Rectangle">
+        <svg className="absolute block inset-0 size-full" fill="none" height="98" preserveAspectRatio="none" viewBox="0 0 194 98" width="194">
+          <g id="Rectangle">
+            <path d={svgPaths.p1b9de9c0} fill="var(--fill-0, white)" />
+            <path d={svgPaths.ped0000} stroke="var(--stroke-0, #252525)" strokeOpacity="0.2" />
+          </g>
+        </svg>
+      </div>
+      <div className="-translate-y-full [word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal h-[24px] justify-end leading-[0] left-[1225px] text-[#252525] text-[15px] top-[120px] w-[117px]">
+        <p className="leading-[normal]">My Profile</p>
+      </div>
+      <div className="-translate-y-full [word-break:break-word] absolute flex flex-col font-['Outfit:Regular',sans-serif] font-normal h-[24px] justify-end leading-[0] left-[1225px] text-[#252525] text-[15px] top-[156px] w-[117px]">
+        <p className="leading-[normal]">Logout</p>
+      </div>
+      <Logo />
+      <ProfileUploadIcon />
+      <div className="absolute h-[36px] left-[695px] shadow-[0px_4px_15px_2px_rgba(0,0,0,0.1)] top-[434px] w-[64px]" data-name="How to Create Text To Image Generator Full Stack SAAS app using React JS 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgHowToCreateTextToImageGeneratorFullStackSaasAppUsingReactJs1} />
+      </div>
+      <Logo1 />
+      <PersonTickIcon />
+      <Group2 />
+    </div>
+  );
+}
